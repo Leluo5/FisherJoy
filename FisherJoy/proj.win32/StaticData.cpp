51d274363670a83d76f102ffb3bf93c7bc71fb8f
Leluo5 <32094887+Leluo5@users.noreply.github.com>
2017-10-28 18:21:03 +0800

    修改
    。

--- FisherJoy/proj.win32/StaticData.cpp
+++ FisherJoy/proj.win32/StaticData.cpp
@@ -15,42 +15,48 @@
 using namespace std;
 using namespace cocos2d;
 
+static StaticData* g_sharedStaticData = NULL;
 StaticData* StaticData::sharedStaticData() {
-	
+	if(g_sharedStaticData == NULL)
+	{
+		g_sharedStaticData = new StaticData;
+		g_sharedStaticData->init();
+	}
+	return g_sharedStaticData;
 }
 
 bool StaticData::init() {
 	_dictionary = CCDictionary::createWithContentsOfFile(_staticFileName.c_str());
-	_dictionary.retain();
+	_dictionary->retain();
 	return true;
 }
 
 const char* StaticData::stringFromKey(const std::string &key) {
-	return _dictionary.valueForKey(key)->getCString();
+	return _dictionary->valueForKey(key)->getCString();
 }
 
 int StaticData::intFromKey(const std::string &key) {
-	return _dictionary.valueForKey(key)->intValue();
+	return _dictionary->valueForKey(key)->intValue();
 }
 
 int StaticData::floatFromKey(const std::string &key) {
-	return _dictionary.valueForKey(key)->floatValue();
+	return _dictionary->valueForKey(key)->floatValue();
 }
 
 bool StaticData::booleanFromKey(const std::string &key) {
-	return _dictionary.valueForKey(key)->boolValue();
+	return _dictionary->valueForKey(key)->boolValue();
 }
 
 cocos2d::CCPoint StaticData::pointFromKey(const std::string &key) {
-	return CCPointFromString(_dictionary.valueForKey(key)->getCString());
+	return CCPointFromString(_dictionary->valueForKey(key)->getCString());
 }
 
 cocos2d::CCRect StaticData::rectFromKey(const std::string &key) {
-	return CCRectFromString(_dictionary.valueForKey(key)->getCString());
+	return CCRectFromString(_dictionary->valueForKey(key)->getCString());
 }
 
 cocos2d::CCSize StaticData::sizeFromKey(const std::string &key) {
-	return CCSizeFromString(_dictionary.valueForKey(key)->getCString());
+	return CCSizeFromString(_dictionary->valueForKey(key)->getCString());
 }
 
 void StaticData::purge() {
@@ -61,7 +67,7 @@ StaticData::StaticData() {
 	/*CC_SAFE_RELEASE_NULL(_dictionary);*/
 	if(_dictionary != NULL)
 	{
-	_dictionary.release();
+	_dictionary->release();
 	_dictionary;
 	}
 }
