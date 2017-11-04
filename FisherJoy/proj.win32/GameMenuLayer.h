7a30b7279c195ead3818dce69c205953a4c835b3
Leluo5 <32094887+Leluo5@users.noreply.github.com>
2017-11-04 23:56:17 +0800

    。
    .

--- FisherJoy/proj.win32/GameMenuLayer.h
+++ FisherJoy/proj.win32/GameMenuLayer.h
@@ -1,16 +1,7 @@
 #pragma once
-#include "cocos2d.h"
-using namespace cocos2d;
-
-class GameMenuLayer:public CCLayer
+class GameMenuLayer
 {
 public:
 	GameMenuLayer(void);
-	CREATE_FUNC(GameMenuLayer)
-	virtual bool init();
-	virtual ~GameMenuLayer(void);
-protected:
-	void createMenu(void);
-	void menuCallbackStartGame(CCObject *sender);
 };
 
